8b0bc67d9a5a193c9334b0c60189bb3ea6d70492
walterseewald <walterseewald@192.168.0.103>
2013-09-11 19:07:36 -0300

    Commit WS

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Panel\CtrlwBundle\PanelCtrlwBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
