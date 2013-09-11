48fd3294f188d60c38f960550bdff0bc825191aa
walterseewald <walterseewald@192.168.0.103>
2013-09-11 19:31:46 -0300

    Commit WS

---
+++ src/Panel/CtrlwBundle/Entity/Usuarios.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Panel\CtrlwBundle\Entity;
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Usuarios
+ *
+ * @ORM\Table(name="usuarios")
+ * @ORM\Entity
+ */
+class Usuarios extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+     
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
